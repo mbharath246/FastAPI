22ecf4dc8f81d69ed21cf3de40d2fed7cb00a5a8
mbharath246 <bharathroyal246@gmail.com>
2024-03-19 17:34:05 +0530

    added

--- project/routers/JWTtoken.py
+++ project/routers/JWTtoken.py
@@ -19,7 +19,7 @@ def create_access_token(data: dict):
 
 def verify_token(token:str, credentials_exception):
     try:
-        pay ad = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
